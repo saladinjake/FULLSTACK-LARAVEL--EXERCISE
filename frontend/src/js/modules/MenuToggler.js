82f4b87f904668e67dc7a123cc72b686b48e9186
saladinjake <juwavictor@gmail.com>
2022-05-23 15:10:08 +0100

    bg: fixes for menu toggler

--- frontend/src/js/modules/MenuToggler.js
+++ frontend/src/js/modules/MenuToggler.js
@@ -1,50 +1,70 @@
 const getDom = (expr) =>{
 	return document.querySelector(expr);
 }
+
+var open = true;
+
+getDom('.open-left').addEventListener('click',function() {
+  if(!open) {
+    showMenu();
+  }
+  else {
+    hideMenu();
+  }
+});
+
+// getDom('nav a').click(function() {
+//   hideMenu();
+// });
+
+function hideMenu() {
+  getDom('.entity2').style.width=0;
+  getDom('.content-page').style.width =getDom('.content-page').style.width+ 250;
+  open = false;
+};
+
+function showMenu() {
+  getDom('.entity2').style.width="240px";
+  getDom('.content-page').classList.add('menu-push-right');
+  open = true;
+};
 class MenuToggler{
 	constructor(){
-	  this.$body = getDom('body');
-      this.$openLeftBtn = getDom('.open-left');
-      this.$menuItem = getDom('#sidebar-menu a');
+	 
 	}
 
    openLeftBar = () =>{
-    getDom('#wrapper').classList.toggle('enlarged');
-    getDom('#wrapper').classList.add('forced');
+    //this.$menuItem..display=""
+    // getDom('#wrapper').classList.toggle('enlarged');
+    // getDom('#wrapper').classList.add('forced');
 
-    if (getDom('#wrapper').classList.contains('enlarged') && getDom('body').classList.contains('fixed-left')) {
-      getDom('body')
-        .classList.remove('fixed-left')
-        getDom('body')
-        .classList.add('fixed-left-void');
-    } else if (!getDom('#wrapper').classList.contains('enlarged') && getDom('body').classList.contains('fixed-left-void')) {
-      getDom('body')
-        .classList.remove('fixed-left-void')
-      getDom('body')
-        .classList.add('fixed-left');
-    }
-
-    if (getDom('#wrapper').classList.contains('enlarged')) {
-      getDom('.left ul').removeAttribute('style');
-    } else {
-      // getDom('.subdrop')
-      //   .siblings('ul:first')
-      //   .show();
-    }
+    // if (getDom('#wrapper').classList.contains('enlarged') && getDom('body').classList.contains('fixed-left')) {
+    //   getDom('body')
+    //     .classList.remove('fixed-left')
+    //     getDom('body')
+    //     .classList.add('fixed-left-void');
+    // } else if (!getDom('#wrapper').classList.contains('enlarged') && getDom('body').classList.contains('fixed-left-void')) {
+    //   getDom('body')
+    //     .classList.remove('fixed-left-void')
+    //   getDom('body')
+    //     .classList.add('fixed-left');
+    // }
 
     
-    // getDom('body').trigger('resize');
+  
+    
+    // getDom('body').classList.add('enlarged');
   }
 
   attachEvents(){
-  	var ua = navigator.userAgent,
-    event = ua.match(/iP/i) ? 'touchstart' : 'click';
-    let $this = this;
-      //bind on click
-    this.$openLeftBtn.addEventListener(event, function(e) {
-      e.stopPropagation();
-      $this.openLeftBar();
-    });
+  	// var ua = navigator.userAgent,
+   //  event = ua.match(/iP/i) ? 'touchstart' : 'click';
+   //  let $this = this;
+   //    //bind on click
+   //  this.$openLeftBtn.addEventListener(event, function(e) {
+   //    e.stopPropagation();
+   //    $this.openLeftBar();
+   //  });
   }
 }
 
